35f557f6f3a196dcce0c429c33ce098a65e5f58a
Daniel Toro soto <daniel.toro1@utp.edu.co>
2025-04-09 01:12:17 -0500

    nueva ruta de productos

--- modules/api/controllers/main.py
+++ modules/api/controllers/main.py
@@ -12,3 +12,15 @@ class UserAPI(http.Controller):
             'login': u.login,
             'email': u.email or '',
         } for u in users]
+    
+    @http.route('/api/products', auth='public', type='json', csrf=False)
+    def get_products(self):
+        products = request.env['inventory.product'].sudo().search([], limit=50)
+        return [{
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'quantity': p.quantity,
+            'image': p.image
+            # Agrega aquí los campos que realmente tenga tu modelo
+        } for p in products]
