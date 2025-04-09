370f9b9682e29f0b94e9181a2a624f59c67f2d43
Daniel Toro soto <daniel.toro1@utp.edu.co>
2025-04-08 21:54:12 -0500

    modulo inventario products

---
+++ modules/inventory_quindicolors/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "QuindiColors",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/products_views.xml",
+        "views/products_menus.xml"
+    ],
+    "application": True,
+}
---
+++ modules/inventory_quindicolors/models/products.py
@@ -0,0 +1,18 @@
+from odoo import fields, models
+
+class Products(models.Model):
+    _name = "inventory.product"
+    _description = 'Products of the inventory'
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ("out_of_stock", "Out of Stock"),
+        ("discontinued", "Discontinued"),
+        ("active", "Active"),
+    ], default="active", string="Status", copy=False)
+    price = fields.Float(string="Price", required=True)
+    quantity = fields.Integer(string="Quantity", required=True)
+    date_created = fields.Datetime(string="Date Created", default=fields.Datetime.now, readonly=True)
+    provider_price = fields.Float(string="Provider Price", required=True)
